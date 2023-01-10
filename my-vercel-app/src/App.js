54e10d1caba159b7a03d4ea1d1c000c2db128882
ThierryCohnen <thierrycohnen@gmail.com>
2023-01-10 18:40:02 +0100

    first

--- my-vercel-app/src/App.js
+++ my-vercel-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React avec moi
+          Learn React avec toi et moi.
         </a>
       </header>
     </div>
